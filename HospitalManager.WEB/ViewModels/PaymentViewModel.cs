3718ed3038ad6ac5bb79c93ce8b2ac44ca17ed0d
Serhii Iordanov <serhii_iordanov@epam.com>
2017-03-09 12:30:01 +0200

    add json properties to payment model

--- HospitalManager.WEB/ViewModels/PaymentViewModel.cs
+++ HospitalManager.WEB/ViewModels/PaymentViewModel.cs
@@ -1,15 +1,21 @@
-﻿using HospitalManager.Core.Enums;
+﻿using System;
+using HospitalManager.Core.Enums;
+using Newtonsoft.Json;
 
 namespace HospitalManager.WEB.ViewModels
 {
+    [Serializable]
     public class PaymentViewModel
     {
         public int Id { get; set; }
 
+        [JsonProperty("order")]
         public string Signature { get; set; }
 
+        [JsonProperty("amt")]
         public decimal Sum { get; set; }
 
+        [JsonProperty("ccy")]
         public string Currency { get; set; }
 
         public string Details { get; set; }
