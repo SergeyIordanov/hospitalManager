3ec2f8a2d83358e805a407188db91e6d5d68e7b5
Blackstar <13heavyweather@gmail.com>
2017-03-14 02:07:39 +0200

    Added an entropy generator

--- HospitalManager.Core/Encryption/Entropy.cs
+++ HospitalManager.Core/Encryption/Entropy.cs
@@ -1,7 +1,17 @@
-﻿namespace HospitalManager.Core.Encryption
+﻿using System.Security.Cryptography;
+
+namespace HospitalManager.Core.Encryption
 {
     public static class Entropy
     {
+        public static byte[] CreateRandomEntropy()
+        {
+            var entropy = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(entropy);
+
+            return entropy;
+        }
+
         public static readonly byte[] EntropyBytes = { 1, 2, 3, 4, 5 };
     }
 }
